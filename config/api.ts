0c2572534d8018375f35951f72528d54e1f41c21
Bacococc <pphj7353@naver.com>
2026-01-08 15:35:10 +0900

    chore: clean up root structure for submodule

--- config/api.ts
+++ config/api.ts
@@ -6,6 +6,8 @@ export const API_CONFIG = {
   // 소켓 서버 주소
   SOCKET_URL: "http://localhost:8000",
 
-  // 서버 사이드 (Next.js 서버)에서 도커 내부 네트워크로 접근할 때 사용하는 주소
-  INTERNAL_BASE_URL: "http://backend:3000",
+  // 서버 사이드 (Next.js 서버)에서 접근할 때 사용하는 주소
+  // 도커 브라우저 환경이 아닐 때는 localhost:8000 쓰셈!
+  // 도커 내부 네트워크라면 http://backend:3000 사용
+  INTERNAL_BASE_URL: "http://localhost:8000",
 };
