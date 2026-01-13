8faecf57b504d67b99c53179d983bd44e13cd184
seulzutora <seulzutora@gmail.com>
2026-01-12 21:18:27 -0800

    ✨ SearchStore과 StoreInfo를 분리
    searchStore, storeInfo로 분리하여 마트 검색과 실제 장소 정보의 상태 분

--- components/createParty/PartyInput.tsx
+++ components/createParty/PartyInput.tsx
@@ -3,17 +3,21 @@
 interface Props {
   label: string;
   name?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   className?: string;
 }
 
-export default function PartyInput({ label, name, placeholder, className }: Props) {
+export default function PartyInput({ label, name, value, placeholder, onChange, className }: Props) {
   return (
     <div className={`flex items-center ${className || 'mb-10'}`}>
       <label className="w-49 font-medium text-gray-700">{label}</label>
       <input 
         type="text" 
         name={name}
+        value={value}
+        onChange={onChange}
         placeholder={placeholder}
         className="flex-1 border border-gray-300 rounded-lg p-3 focus:outline-none focus:border-green-700 bg-white"
       />
--- components/createParty/PartyLocation.tsx
+++ components/createParty/PartyLocation.tsx
@@ -4,14 +4,30 @@ import { useState } from 'react';
 import PartyInput from "./PartyInput";
 
 export default function GroupLocation() {
-  const [martInfo, setMartInfo] = useState({
-    name: '',
-    address: ''
-  });
+  const [searchStore, setSearchStore] = useState('');
+  const [storeInfo, setStoreInfo] = useState<{
+    name: string,
+    address: string,
+    latitude: string,
+    longitude: string,
+  } | null>(null);
 
-  // 나중에 구글 API 등에서 장소를 선택했을 때 이 함수를 실행하게 됩니다.
-  const handleSelectPlace = (selectedName: string, selectedAddress: string) => {
-    setMartInfo({ name: selectedName, address: selectedAddress });
+  // 나중에 kakao API 등에서 장소를 선택했을 때 이 함수를 실행하게 됩니다.
+  const handleSelectPlace = (
+    selectedName: string,
+    selectedAddress: string,
+    selectedLatitude: string,
+    selectedLongitude: string
+  ) => {
+    setStoreInfo({
+      name: selectedName,
+      address: selectedAddress,
+      latitude: selectedLatitude,
+      longitude: selectedLongitude
+    });
+
+    // 검색어를 선택된 마트 이름으로 동기화
+    setSearchStore(selectedName);
   };
 
   return (
@@ -20,13 +36,16 @@ export default function GroupLocation() {
       <PartyInput
         label="마트"
         placeholder="마트 이름을 검색하세요"
+        value={searchStore}
+        onChange={(e) =>
+          setSearchStore(e.target.value)}
         className="mb-2.5"
       />
 
       {/* 2. Hidden Inputs: 실제 백엔드로 보낼 마트 이름과 주소를 담는 곳 */}
-      {/* value가 martInfo 상태와 연결되어 있어, 지도 검색 시 자동으로 업데이트됨 */}
-      <input type="hidden" name="martName" value={martInfo.name} />
-      <input type="hidden" name="martAddress" value={martInfo.address} />
+      {/* value가 storeInfo 상태와 연결되어 있어, 지도 검색 시 자동으로 업데이트됨 */}
+      <input type="hidden" name="store_name" value={storeInfo?.name} />
+      <input type="hidden" name="address" value={storeInfo?.address} />
 
       <div className="flex">
         <div className="w-49" /> 
@@ -37,11 +56,15 @@ export default function GroupLocation() {
 
           <div className="flex items-center gap-2">
             <span className="font-bold text-lg text-gray-900">
-              {martInfo.name || '(마트 이름)'}
+              {storeInfo?.name || '(마트 이름)'}
             </span>
             <span className="text-gray-500 text-sm">
-              {martInfo.address || '(마트 주소)'}
+              {storeInfo?.address || '(마트 주소)'}
             </span>
+
+            <input type="hidden" name="latitude" value={storeInfo?.latitude} />
+            <input type="hidden" name="longitude" value={storeInfo?.longitude} />
+
           </div>
         </div>
       </div>
